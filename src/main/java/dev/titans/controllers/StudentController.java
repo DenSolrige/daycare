e339fb34f4511c0c1166f473cf52b981c33c9026
Sunny <faronwmoore@gmail.com>
2022-09-01 11:45:08 -0500

    Swapped from response body to rest controller in student controller

--- src/main/java/dev/titans/controllers/StudentController.java
+++ src/main/java/dev/titans/controllers/StudentController.java
@@ -4,27 +4,22 @@ import dev.titans.entities.Student;
 import dev.titans.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RestController
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
     @DeleteMapping("/students/{id}")
-    @ResponseBody
     public void deleteStudentById(@RequestParam String id){
         int s_id = Integer.parseInt(id);
         this.studentService.deleteStudentById(s_id);
     }
 
     @PostMapping("/student")
-    @ResponseBody
     public Student createStudent(@RequestBody Student student){
         return this.studentService.createStudent(student);
     }
