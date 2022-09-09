93db32d98cbdba9dedddfe9ba24a878c56bf60ed
Myles0911 <mdwalker1@aggies.ncat.edu>
2022-09-09 09:16:07 -0400

    Messaging

--- src/test/java/dev/titans/daycare/DaycareApplicationTests.java
+++ src/test/java/dev/titans/daycare/DaycareApplicationTests.java
@@ -1,14 +1,28 @@
 package dev.titans.daycare;
 
+
+import dev.titans.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DaycareApplicationTests {
 
+	@Autowired
+	JwtService jwtServicel;
+
 	@Test
 	void contextLoads() {
+
 	}
+
+	@Test
+	void validates_jwt() {
+		String jwt = "fseesad";
+		jwtServicel.validateJwt(jwt);
+	}
+
 
 
 
